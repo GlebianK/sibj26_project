2bf3594a79570180492ab52de15e7d61279d882c
Anton Egorov <azzaghtoth@gmail.com>
2026-05-02 23:11:56 +0300

    animation fixes and bear punch

--- Assets/Scripts/Debug/GameDebugTool.cs
+++ Assets/Scripts/Debug/GameDebugTool.cs
@@ -5,17 +5,26 @@ public class GameDebugTool : MonoBehaviour
 {
     [SerializeField] private InputActionReference _startGameActionReference;
     [SerializeField] private InputActionReference _endGameActionReference;
+    [SerializeField] private InputActionReference _punchTestActionReference;
 
     private void Awake()
     {
         _startGameActionReference.action.performed += StartGame;
         _endGameActionReference.action.performed += EndGame;
+        _punchTestActionReference.action.performed += TestPunch;
     }
+
 
     private void OnDestroy()
     {
         _startGameActionReference.action.performed -= StartGame;
         _endGameActionReference.action.performed -= EndGame;
+        _punchTestActionReference.action.performed -= TestPunch;
+    }
+
+    private void TestPunch(InputAction.CallbackContext obj)
+    {
+        PlayerController.Instance.Punch();
     }
 
     private void StartGame(InputAction.CallbackContext obj)
